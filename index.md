d7f594ea85cba6a0a9397d2bc0bed0b7ae842b5b
Sophie Stadler <srs2231@columbia.edu>
2016-03-13 18:01:25 -0400

    v3 release

---
+++ index.md
@@ -0,0 +1,27 @@
+---
+title: Sophie Stadler
+layout: page
+---
+
+I’m a sophomore studying computer science at Columbia University. This summer, I’ll be interning at an NYC startup as a [hackNY Fellow](http://hackny.org/a/fellows/).
+
+Here’s how I’m spending my time lately –
+
+1. Building things with [3005hacks](http://3005hacks.com)
+2. Recording [Good Content](http://goodcontentpodcast.com)
+3. Working on [cs3157](http://www.cs.columbia.edu/~jae/3157/)
+
+Some things I also enjoy –
+
+1. [Typography](https://github.com/sophstad/typelinks)
+2. Collecting and analyzing [personal data](http://sophiestadler.com/france)
+3. Bagels
+4. Lists
+
+Another list –
+
+1. [Email me](mailto:srs2231@columbia.edu)
+2. [Resumé](/resume.pdf)
+3. [GitHub](http://github.com/sophstad)
+4. [Tumblr](http://hypertexts.tumblr.com)
+5. [LinkedIn](http://www.linkedin.com/in/sophiestadler)
---
+++ static/js/background.js
@@ -0,0 +1,40 @@
+var pi = Math.PI;
+var sin = Math.sin;
+var floor = Math.floor;
+var abs = Math.abs;
+var pow = Math.pow;
+
+var epoch = function() { return new Date().getTime()/2000; }
+// The last number in that line controls color cycle speed.
+
+function clip(min, n, max) {
+  if (min > n) { return min; }
+  if (max < n) { return max; }
+  return n;
+}
+
+function K(h) {
+  h *= -1;
+  var r = sin(pi * h);
+  var g = sin(pi * (h + 1/3));
+  var b = sin(pi * (h + 2/3));
+  return [r, g, b].map(function (c) {
+    c = c*c;
+    c = 0.85 + (c*0.1);
+    return floor(c * 255);
+  });
+}
+
+function recolor(plus) {
+  var cycle = ((epoch()/50) + plus) % 1;
+  var color = K(cycle);
+  color = 'rgb(' + color.join(',') + ')';
+  $('body').css({'background-color': color});
+}
+  
+$(document).ready(function(){
+  recolor(0);
+  // The last number in this line is the update interval (in ms):
+  a = function() { setInterval(function() { recolor(0) }, 100); }
+  a();
+});
