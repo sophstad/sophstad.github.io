454de126cfafc5cb8e9fccb40ea2eee497d1e5f6
Sophie Stadler <srs2231@columbia.edu>
2016-10-11 01:00:04 -0400

    Clean up js

--- static/js/background.js
+++ static/js/background.js
@@ -4,31 +4,35 @@ var floor = Math.floor;
 var abs = Math.abs;
 var pow = Math.pow;
 
-var epoch = function() { return new Date().getTime()/2000; }
+var epoch = function () {
+  return Date.now() / 2000;
+};
 // The last number in that line controls color cycle speed.
 
 function K(h) {
   h *= -1;
   var r = sin(pi * h);
-  var g = sin(pi * (h + 1/3));
-  var b = sin(pi * (h + 2/3));
+  var g = sin(pi * (h + 1 / 3));
+  var b = sin(pi * (h + 2 / 3));
   return [r, g, b].map(function (c) {
-    c = c*c;
-    c = 0.85 + (c*0.1);
+    c = c * c;
+    c = 0.85 + (c * 0.1);
     return floor(c * 255);
   });
 }
 
 function recolor(plus) {
-  var cycle = ((epoch()/50) + plus) % 1;
+  var cycle = ((epoch() / 50) + plus) % 1;
   var color = K(cycle);
   color = 'rgb(' + color.join(',') + ')';
   $('body').css({'background-color': color});
 }
-  
-$(document).ready(function(){
+
+$(document).ready(function () {
   recolor(0);
   // The last number in this line is the update interval (in ms):
-  a = function() { setInterval(function() { recolor(0) }, 100); }
+  var a = function() {
+    setInterval(function () { recolor(0); }, 100);
+  };
   a();
 });
