8f26e05f32ab1edffe2f92b19104ee6bf8ba4635
Sophie Stadler <srs2231@columbia.edu>
2016-10-22 01:10:58 -0400

    Fix music toggle

--- static/js/last-fm.js
+++ static/js/last-fm.js
@@ -1,9 +1,9 @@
 function lfmMostRecentTrack(JSONdata) {
   try {
     var oTrack = (new Array().concat(JSONdata.recenttracks.track))[0];
-    if (typeof oTrack.date = "undefined") {
-      var lfm = document.getElementById("lfm");
-      lfm.className = "xs-hide";
+    if (typeof oTrack.date == "undefined") {
+      var lfm = document.getElementById("lfm-trigger");
+      lfm.className = "o2";
     }
     document.getElementById("lfm-artist").innerHTML = oTrack.artist["#text"];
     document.getElementById("lfm-track").innerHTML = oTrack.name;
