626a54f8bf615a96743ebd23408aa5c64127df66
Sophie Stadler <srs2231@columbia.edu>
2016-10-21 23:58:18 -0400

    Fix music widget

--- static/js/last-fm.js
+++ static/js/last-fm.js
@@ -2,8 +2,8 @@ function lfmMostRecentTrack(JSONdata) {
   try {
     var oTrack = (new Array().concat(JSONdata.recenttracks.track))[0];
     if (typeof oTrack.date != "undefined") {
-      var icon = document.getElementById("lfm");
-      icon.className = "hide";
+      var lfm = document.getElementById("lfm");
+      lfm.className += " hide";
     }
     document.getElementById("lfm-artist").innerHTML = oTrack.artist["#text"];
     document.getElementById("lfm-track").innerHTML = oTrack.name;
