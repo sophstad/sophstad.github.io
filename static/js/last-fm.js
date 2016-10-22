005134b09f83fec556274ffac639ee21ab6438a7
Sophie Stadler <srs2231@columbia.edu>
2016-10-22 00:03:11 -0400

    Switch from auto-show to auto-hide

--- static/js/last-fm.js
+++ static/js/last-fm.js
@@ -1,9 +1,9 @@
 function lfmMostRecentTrack(JSONdata) {
   try {
     var oTrack = (new Array().concat(JSONdata.recenttracks.track))[0];
-    if (typeof oTrack.date != "undefined") {
+    if (typeof oTrack.date = "undefined") {
       var lfm = document.getElementById("lfm");
-      lfm.className += " hide";
+      lfm.className = "xs-hide";
     }
     document.getElementById("lfm-artist").innerHTML = oTrack.artist["#text"];
     document.getElementById("lfm-track").innerHTML = oTrack.name;
